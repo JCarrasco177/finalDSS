e737f85d31c68b9efa754b9bd3b47f9169c93280
Antonio Salinas R <antonio.salinasr@gmail.com>
2022-04-06 19:22:40 -0400

    clase 5

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const express = require('express')
 const app = express()
+es6Renderer = require('express-es6-template-engine'),
+
+
+app.engine('html', es6Renderer);
+app.set('views', 'views');
+app.set('view engine', 'html');
 
 //Rutas
-app.get('/hola',(request , response)=>{
-    response.send('Hello  wolrd!!!')
+app.get('/index',(request , response)=>{
+    response.render('index');
 })
 
-app.post('/getName',(request , response)=>{
-    response.json({
-        nombre: "Antonio Salinas",
-        direccion: "Mi Casa 3344",
-        ciudad: "Valparaíso"
-    })
-})
+
 app.post('/getFullName',(request , response)=>{
     response.json({
         nombre: "Antonio Javier Salinas Rodríguez",
