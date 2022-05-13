5e28afd4bf016376f971fe7d18ec0e005c883e78
Antonio Salinas R <antonio.salinasr@gmail.com>
2022-05-12 22:23:54 -0400

    clase 14, con errores

--- controllers/PostsController.js
+++ controllers/PostsController.js
@@ -1,48 +1,57 @@
 const utils = require('../resources/utils')
 const moment = require('moment')
 const connection = require('../config/db')
+var usr 
+var msg
+var pst 
+const getMessage = (request) =>{
 
-const setPost = (request,response) => {
-    //comentario
-    //user_id
-    connection.query(
-        'insert into posts (user_id,title,content) values ("'+request.body.user_id+'","'+request.body.title+'","'+request.body.content+'")',
-        function(err, results, fields) {
-            if (err) {
-                message = "'Hubo un error al insertar el post'"
-            }else{
-                message = "'Se ha insertado exitosamente!!!!'"
-            }
-
-        }
-    );
-    var user = {}
-    connection.query(
-        'select * from users where id ='+request.body.user_id,
-        function(err, results, fields) {
-            if (err) {
-                user = {}
-            }else{
-                user = results[0]
-            }
-
-        }
-    );
-    var posts=[]
-    connection.query(
-        'select * from posts',
-        function(err, results, fields) {
-            if (err) {
-                posts = []
-            }else{
-                posts = results
-            }
-
-        }
-    );
-    response.render('posts',{locals:{message,user,posts}})
- 
+   connection.promise().query('insert into posts (user_id,title,content) values ("'+request.body.user_id+'","'+request.body.title+'","'+request.body.content+'")')
+  .then( ([rows,fields,err]) => {
+    if (err) {
+        msg = "'Hubo un error al insertar el post'"
+    }else{
+        msg = "'Se ha insertado exitosamente!!!!'"
+    }
+  }) 
+  return msg
+    
+} 
+const getUser = (request) =>{
+    console.log('select * from users where id ='+request.body.user_id)
+    connection.promise().query('select * from users where id ='+request.body.user_id)
+    .then( ([rows,fields,err]) => {
+      usr = rows[0]
+      }
+    )
+    return usr
+    
+    
 }
+
+const getPosts = () =>{
+    
+    connection.promise().query('select *, date_format(date,"%d/%m/%Y %h:%I:%S") as fixedate from posts join users on posts.user_id = users.id')
+    .then( ([rows,fields,err]) => {
+      pst = rows
+      }
+    )
+    return pst
+    
+
+}
+
+
+
+const setPost =  async (request,response) => {
+
+    const message = await getMessage(request)
+    const posts = await getPosts(request)
+    const user = await getUser(request)
+    console.log({message,user,posts})
+    response.render('post',{locals:{message,user,posts}})
+}
+
 const post = (request,response) =>{
     //{locals:{user,posts}},
     console.log('llegamos aca')
--- routes/main.js
+++ routes/main.js
@@ -3,17 +3,20 @@ const router = express.Router();
 const utils = require('../resources/utils')
 const main = require('../controllers/MainController')
 const users = require('../controllers/UsersController')
+const post = require('../controllers/PostsController')
 
 //Rutas
 //MainController
 router.get('/index',main.getIndex)
 router.get('/post',main.getPost)
-router.post('/setpost',main.setPost)
+
 //UsersController
 router.get('/testmysql',users.testMysql)
 router.post('/createuser',users.createUser)
 router.post('/getuserbyid',users.getUserById)
 router.post('/login',users.setLogin)
 router.get('/avatar',users.getAvatar)
+//post controller
+router.post('/setpost',post.setPost)
 
 module.exports = router;
